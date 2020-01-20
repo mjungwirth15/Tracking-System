bb47727e314bd3a38c61b62f3380b50f9feeee3c
mjung <maci2000.12@gmail.com>
2020-01-20 11:13:20 +0100

    Projekt erstellt

---
+++ B_Versionierung_Git_C_Uebung/app/src/main/java/com/example/b_versionierung_git_c_uebung/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.b_versionierung_git_c_uebung;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
